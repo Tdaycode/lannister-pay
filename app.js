ef82459f2fac33642d41334d11a6ab193999c030
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-03-17 22:54:57 +0100

    testing app

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: "500mb", extended: true }));
 app.use(cors());
 
 // app.use("/api/v1/pay", paymentRoutes);
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.set("Content-Type", "text/json");
   res.status(200).send("app is working ");
 });

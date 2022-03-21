77dc7e2d035ff1bffaac6c22d1228360ef6cd6f4
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-03-21 14:02:53 +0100

    fxing compute route

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -19,10 +19,11 @@ const feeConfig =asyncHandler(async(req, res) => {
 console.log("here")
   const {FeeConfigurationSpec} = req.body;
   try {
+    if(!FeeConfigurationSpec) res.status(403).send("FeeConfiguration is required");
     
-  let feeConfig = FeeConfigurationSpec.split('\n').map((item)=> {
-  let feeItem = item.split(' ');
-  let FEE_ID = feeItem[0]
+    let feeConfig = FeeConfigurationSpec.split('\n').map((item)=> {
+    let feeItem = item.split(' ');
+    let FEE_ID = feeItem[0]
   return {
 
       [FEE_ID]:{
@@ -38,7 +39,7 @@ console.log("here")
 })
 
     feeConfig = Object.assign({},...feeConfig)
-    await client.set('foo', JSON.stringify(feeConfig));
+    await client.set('feeConfig', JSON.stringify(feeConfig));
 
 
     res.status(200).json({
@@ -55,13 +56,64 @@ console.log("here")
   }
 })
 
+/**
+ *@desc    HTTP POST  
+ *@route   HTTP POST /compute-transaction-fee
+ *@access  Public
+ */
+
 const transactFee = asyncHandler(async (req, res) => {
+  feeConfig = {
+    LNPY1221: {
+      FEE_CURRENCY: 'NGN',
+      FEE_LOCALE: '*',
+      FEE_ENTITY: '*(*)',
+      FEE_TYPE: 'PERC',
+      FEE_VALUE: '1.4'
+    },
+    LNPY1222: {
+      FEE_CURRENCY: 'NGN',
+      FEE_LOCALE: 'INTL',
+      FEE_ENTITY: 'CREDIT-CARD(VISA)',
+      FEE_TYPE: 'PERC',
+      FEE_VALUE: '5.0'
+    },
+    LNPY1223: {
+      FEE_CURRENCY: 'NGN',
+      FEE_LOCALE: 'LOCL',
+      FEE_ENTITY: 'CREDIT-CARD(*)',
+      FEE_TYPE: 'FLAT_PERC',
+      FEE_VALUE: '50:1.4'
+    },
+    LNPY1224: {
+      FEE_CURRENCY: 'NGN',
+      FEE_LOCALE: '*',
+      FEE_ENTITY: 'BANK-ACCOUNT(*)',
+      FEE_TYPE: 'FLAT',
+      FEE_VALUE: '100'
+    },
+    LNPY1225: {
+      FEE_CURRENCY: 'NGN',
+      FEE_LOCALE: '*',
+      FEE_ENTITY: 'USSD(MTN)',
+      FEE_TYPE: 'PERC',
+      FEE_VALUE: '0.55'
+    }
+  }
 
   try {
-    const value =  await client.get('foo')
-    console.log(JSON.parse(value))
-    res.send(JSON.parse(value))
+    const {ID, Amount, Currency, CurrencyCountry, Customer, PaymentEntity} = req.body;
+    if(!Amount) res.status(403).send('Amount cannot be negative');
+    let feeConfig =  await client.get('feeConfig')
+    feeConfig = JSON.parse(feeConfig)
+    const keys = Object.keys(feeConfig)
+    console.log(keys)
+    keys.forEach((key, index) => {
+      let feeValue = feeConfig[key].FEE_VALUE;
+      console.log(typeof parseInt(feeCur))
+    });
     
+    console.log(feeConfig)
   }catch (err) {
     return res.status(500).json({
       status: "error",
